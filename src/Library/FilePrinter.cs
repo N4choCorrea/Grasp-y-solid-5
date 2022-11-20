f73b8924b059d34a9e8d1a8372ed42dd99b8337c
N4choCorrea <correa.ignacio48@gmail.com>
2022-11-20 17:31:59 -0300

    resuelto

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class FilePrinter : IPrinter
     {
-        public void PrintRecipe(IRecipeContent recipeContent)
+        public void PrintRecipe(IRecipeContent recipe)
         {
-            File.WriteAllText("Recipe.txt", recipeContent.GetTextToPrint());
+            File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
         }
         
     }
